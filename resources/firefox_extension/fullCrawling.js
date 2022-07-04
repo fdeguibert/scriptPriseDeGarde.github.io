2811b63624f766f8ef4cf9522f37d3ebbb9654eb
Deguibert <francois.deguibert@mirakl.com>
2022-07-04 19:03:57 +0200

    full crawl no dedoublon

--- resources/firefox_extension/fullCrawling.js
+++ resources/firefox_extension/fullCrawling.js
@@ -1,3 +1,7 @@
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 function getHtmlDocumentFromLocal(url) {
     const xmlHttp = new XMLHttpRequest();
     xmlHttp.open("GET", url, false);
@@ -20,19 +24,33 @@ function getDataForContext(context) {
     return el;
 }
 
-function main() {
-    const startingDate = new Date('2022-06-27T03:24:00')
-    console.log('start crawling from date : ' + startingDate.toLocaleDateString('fr'));
-    const context = getHtmlDocumentFromLocal(`https://portail.sdis78.fr/jcms/p_1295618/cs-chevreuse?portlet=p_1336294&dateMci=${startingDate.toLocaleDateString('fr').replace('-', '/')}`).getElementsByTagName('body')[0].getAttribute('id');
-    console.log(context);
+// async function main() {
+ function main() {
+    const dateProcessed = new Date('2022-06-27T03:24:00');
+    let totalInters = 0;
+    let totalInfos = 0;
 
-    const dataForDay = getDataForContext(context);
+    console.log(dateProcessed)
+    while(dateProcessed.getFullYear()===2022){
+        console.log('start crawling from date : ' + dateProcessed.toLocaleDateString('fr'));
+        const context = getHtmlDocumentFromLocal(`https://portail.sdis78.fr/jcms/p_1295618/cs-chevreuse?portlet=p_1336294&dateMci=${dateProcessed.toLocaleDateString('fr').replace('-', '/')}`).getElementsByTagName('body')[0].getAttribute('id');
+        console.log(context);
 
-    //Comptage basic des inters du jour (attention, prend pas en compte les inters de la veille au soir, pas de verif des dates etc...
-    let dailyInterventions = dataForDay.getElementsByClassName('inter').length;
-    let dailyInfos = dataForDay.getElementsByClassName('nonreponse').length;
-    console.log(`dailyInterventions : ${dailyInterventions}`);
-    console.log(`dailyInfos : ${dailyInfos}`);
+        const dataForDay = getDataForContext(context);
+
+        //Comptage basic des inters du jour (attention, prend pas en compte les inters de la veille au soir, pas de verif des dates etc...
+        let dailyInterventions = dataForDay.getElementsByClassName('inter').length;
+        let dailyInfos = dataForDay.getElementsByClassName('nonreponse').length;
+        console.log(`dailyInterventions : ${dailyInterventions}`);
+        console.log(`dailyInfos : ${dailyInfos}`);
+        totalInters += dailyInterventions;
+        totalInfos += dailyInfos;
+        // await sleep(1000); // avoid bruteforce detection
+        dateProcessed.setDate(dateProcessed.getDate() - 1);
+    }
+    console.log(`* total inters: ${totalInters}`)
+    console.log(`* total infos: ${totalInfos}`)
+
 }
 
 main();
