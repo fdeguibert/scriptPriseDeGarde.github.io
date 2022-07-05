b9c047072047ae4aab56ae90df7fe182bc659670
Deguibert <francois.deguibert@mirakl.com>
2022-07-05 12:14:46 +0200

    seems good, missing full rebuild button

--- resources/firefox_extension/fullCrawling.js
+++ resources/firefox_extension/fullCrawling.js
@@ -1,7 +1,3 @@
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
 function getHtmlDocumentFromLocal(url) {
     const xmlHttp = new XMLHttpRequest();
     xmlHttp.open("GET", url, false);
@@ -9,6 +5,7 @@ function getHtmlDocumentFromLocal(url) {
     const htmlResult = xmlHttp.responseText;
     const el = document.createElement('html');
     el.innerHTML = htmlResult;
+
     return el;
 }
 
@@ -24,34 +21,60 @@ function getDataForContext(context) {
     return el;
 }
 
-// async function main() {
- function main() {
-    const dateProcessed = new Date('2022-06-27T03:24:00');
+function generateMessageFromDate(fromDate) {
+    const stopDate = new Date();
+    stopDate.setDate(stopDate.getDate()+1);
+    const dateProcessed = fromDate;
+    let dayYearBefore = true;
     let totalInters = 0;
     let totalInfos = 0;
-
-    console.log(dateProcessed)
-    while(dateProcessed.getFullYear()===2022){
-        console.log('start crawling from date : ' + dateProcessed.toLocaleDateString('fr'));
+    let dayBeforeEvents = [];
+    let dailyInterventions = 0;
+    let dailyInfos = 0;
+    while (!(dateProcessed.getDate() === stopDate.getDate() && dateProcessed.getMonth() === stopDate.getMonth())) {
+        console.log('crawling for date : ' + dateProcessed.toLocaleDateString('fr'));
+        //TODO ME replace URL with generic one (from current URL)
         const context = getHtmlDocumentFromLocal(`https://portail.sdis78.fr/jcms/p_1295618/cs-chevreuse?portlet=p_1336294&dateMci=${dateProcessed.toLocaleDateString('fr').replace('-', '/')}`).getElementsByTagName('body')[0].getAttribute('id');
-        console.log(context);
+        // console.log(context);
 
         const dataForDay = getDataForContext(context);
+        const divs = dataForDay.getElementsByClassName('sdis78box');
+        const dailyEvents = [];
+        for (let i = 0; i < divs.length; i++) {
+            let divClasses = divs[i].querySelector('div:nth-child(1)').className;
+            if (divClasses === 'panel inter' || divClasses === 'panel nonreponse') {
+                dailyEvents.push(divClasses + ' ' + divs[i].querySelector('div:nth-child(1) > div:nth-child(2)> div:nth-child(1) > div:nth-child(1)> div:nth-child(1)> p:nth-child(1)').textContent)
+            }
+        }
+        // console.log(dailyEvents);
+        const distinctDailyEvents = dailyEvents.filter((title) => !dayBeforeEvents.includes(title));
 
-        //Comptage basic des inters du jour (attention, prend pas en compte les inters de la veille au soir, pas de verif des dates etc...
-        let dailyInterventions = dataForDay.getElementsByClassName('inter').length;
-        let dailyInfos = dataForDay.getElementsByClassName('nonreponse').length;
-        console.log(`dailyInterventions : ${dailyInterventions}`);
-        console.log(`dailyInfos : ${dailyInfos}`);
-        totalInters += dailyInterventions;
-        totalInfos += dailyInfos;
-        // await sleep(1000); // avoid bruteforce detection
-        dateProcessed.setDate(dateProcessed.getDate() - 1);
+        dailyInterventions = distinctDailyEvents.filter(title => title.startsWith('panel inter')).length;
+        dailyInfos = distinctDailyEvents.filter(title => title.startsWith('panel nonreponse')).length;
+
+
+        // Comptage basic des inters du jour (attention, prend pas en compte les inters de la veille au soir, pas de verif des dates etc...
+        // let dailyInterventions = dataForDay.getElementsByClassName('inter').length;
+        // let dailyInfos = dataForDay.getElementsByClassName('nonreponse').length;
+        // console.log(`dailyInterventions : ${dailyInterventions}`);
+        // console.log(`dailyInfos : ${dailyInfos}`);
+        if (!dayYearBefore) {
+            console.log(`inters comptabilisées : ${dailyInterventions}`)
+            console.log(`infos comptabilisées : ${dailyInfos}`)
+            totalInters += dailyInterventions;
+            totalInfos += dailyInfos;
+        }else{
+            dayYearBefore = false;
+        }
+        dayBeforeEvents = dailyEvents;
+        dateProcessed.setDate(dateProcessed.getDate() + 1);
     }
-    console.log(`* total inters: ${totalInters}`)
-    console.log(`* total infos: ${totalInfos}`)
+    console.log(`aujourd'hui, inters: ${dailyInterventions}`)
+    console.log(`aujourd'hui, infos: ${dailyInfos}`)
+    console.log(`* total inters comptabilisées: ${totalInters}`)
+    console.log(`* total infos comptabilisées: ${totalInfos}`)
 
 }
 
-main();
+generateMessageFromDate(new Date('2021-12-31T03:03:03Z'));
 
--- resources/firefox_extension/generateSignEndGuard.js
+++ resources/firefox_extension/generateSignEndGuard.js
@@ -25,11 +25,92 @@ function loadEndGuardScript(wasAlreadyDisplayed) {
     if (isDisplayed && !wasAlreadyDisplayed) {
         document.querySelector('div.form-group:nth-child(1) > div:nth-child(2) > div:nth-child(1) > input:nth-child(1)').value = 'Fin de garde du ' + dayBefore.toLocaleDateString('fr');
         textArea.style.height = '170px';
-        fetchAndFillDatasSignEndGuard(textArea);
+        // TODO ME reactivate:  fetchAndFillDatasSignEndGuard(textArea);
+        const latestValidSignature = getLatestValidSignature();
+        console.log(latestValidSignature);
+        const fromDate = latestValidSignature.lastValidSignDate;
+        fromDate.setDate(latestValidSignature.lastValidSignDate.getDate()-1)
+        console.log('from date ' + fromDate.toLocaleDateString('fr'))
+        const eventsFromLatestValidSignature = countEventsFromDate(fromDate)
+        console.log(eventsFromLatestValidSignature)
+        const textSign = buildNewSignText(latestValidSignature, eventsFromLatestValidSignature);
+        console.log(textSign);
     }
     setTimeout(function () {
         loadEndGuardScript(isDisplayed);
     }, 300);
+
+}
+
+function buildNewSignText(latestValidSign, eventsFromLatestValidSign) {
+    let template = "Consignes passées au Chef de Garde montant.\n" +
+        "Interventions : " + eventsFromLatestValidSign.dailyInterventions + "\n" +
+        "Infos : " + eventsFromLatestValidSign.dailyInfos + "\n" +
+        "Cumul interventions : " + (eventsFromLatestValidSign.totalInters + latestValidSign.latestCountInters) + "\n" +
+        "Cumul infos : " + (eventsFromLatestValidSign.totalInfos + latestValidSign.latestCountInfos) + "\n" +
+        "Personnel : " + "\n" +
+        "Moyens : ";
+    return template;
+}
+
+function countEventsFromDate(fromDate) {
+    const stopDate = new Date();
+    stopDate.setDate(stopDate.getDate() + 1);
+    const dateProcessed = fromDate;
+    let dayYearBefore = true;
+    let totalInters = 0;
+    let totalInfos = 0;
+    let dayBeforeEvents = [];
+    let dailyInterventions = 0;
+    let dailyInfos = 0;
+    while (!(dateProcessed.getDate() === stopDate.getDate() && dateProcessed.getMonth() === stopDate.getMonth())) {
+        console.log('crawling for date : ' + dateProcessed.toLocaleDateString('fr'));
+        //TODO ME replace URL with generic one (from current URL)
+        const context = getHtmlDocumentFrom(`https://portail.sdis78.fr/jcms/p_1295618/cs-chevreuse?portlet=p_1336294&dateMci=${dateProcessed.toLocaleDateString('fr').replace('-', '/')}`).getElementsByTagName('body')[0].getAttribute('id');
+        // console.log(context);
+
+        const dataForDay = getDataForContext(context);
+        const divs = dataForDay.getElementsByClassName('sdis78box');
+        const dailyEvents = [];
+        for (let i = 0; i < divs.length; i++) {
+            let divClasses = divs[i].querySelector('div:nth-child(1)').className;
+            if (divClasses === 'panel inter' || divClasses === 'panel nonreponse') {
+                dailyEvents.push(divClasses + ' ' + divs[i].querySelector('div:nth-child(1) > div:nth-child(2)> div:nth-child(1) > div:nth-child(1)> div:nth-child(1)> p:nth-child(1)').textContent)
+            }
+        }
+        // console.log(dailyEvents);
+        const distinctDailyEvents = dailyEvents.filter((title) => !dayBeforeEvents.includes(title));
+
+        dailyInterventions = distinctDailyEvents.filter(title => title.startsWith('panel inter')).length;
+        dailyInfos = distinctDailyEvents.filter(title => title.startsWith('panel nonreponse')).length;
+
+
+        // Comptage basic des inters du jour (attention, prend pas en compte les inters de la veille au soir, pas de verif des dates etc...
+        // let dailyInterventions = dataForDay.getElementsByClassName('inter').length;
+        // let dailyInfos = dataForDay.getElementsByClassName('nonreponse').length;
+        // console.log(`dailyInterventions : ${dailyInterventions}`);
+        // console.log(`dailyInfos : ${dailyInfos}`);
+        if (!dayYearBefore) {
+            console.log(`inters comptabilisées : ${dailyInterventions}`)
+            console.log(`infos comptabilisées : ${dailyInfos}`)
+            totalInters += dailyInterventions;
+            totalInfos += dailyInfos;
+        } else {
+            dayYearBefore = false;
+        }
+        dayBeforeEvents = dailyEvents;
+        dateProcessed.setDate(dateProcessed.getDate() + 1);
+    }
+    console.log(`aujourd'hui, inters: ${dailyInterventions}`)
+    console.log(`aujourd'hui, infos: ${dailyInfos}`)
+    console.log(`* total inters comptabilisées: ${totalInters}`)
+    console.log(`* total infos comptabilisées: ${totalInfos}`)
+    return {
+        dailyInterventions,
+        dailyInfos,
+        totalInters,
+        totalInfos
+    }
 
 }
 
@@ -45,7 +126,7 @@ function fetchAndFillDatasSignEndGuard(textArea) {
     console.log(document.getElementById('divToRefresh').getElementsByClassName('inter').length)
     console.log(dailyInterventions)
 //recherche de la signature de la veille
-    const latestSignatureText = getLatestSignature();
+    const latestSignatureText = getLatestValidSignature();
 
 
     const totalInterventions = calculateNewTotal(latestSignatureText, '.*Cumul.*interventions.*: ([0-9]+).*', dailyInterventions);
@@ -63,44 +144,100 @@ function fetchAndFillDatasSignEndGuard(textArea) {
     textArea.textContent = template;
 }
 
-function getLatestSignature() {
-    const urlForCtxtId = document.querySelector('div.col-xs-2:nth-child(2) > a:nth-child(1)').getAttribute('href');
-
-    let xmlHttpCtxtId = new XMLHttpRequest();
-    xmlHttpCtxtId.open("GET", urlForCtxtId, false);
-    xmlHttpCtxtId.send(null);
-    let htmlResult = xmlHttpCtxtId.responseText;
-    let el = document.createElement('html');
+function getHtmlDocumentFrom(url) {
+    const xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", url, false);
+    xmlHttp.send(null);
+    const htmlResult = xmlHttp.responseText;
+    const el = document.createElement('html');
     el.innerHTML = htmlResult;
 
-    let previousDayCtxtId = el.getElementsByTagName('body')[0].id;
+    return el;
+}
 
+function getDataForContext(context) {
 
-    console.log(previousDayCtxtId);
+    const xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", 'https://portail.sdis78.fr/plugins/MainCourantePlugin/jsp/doMciList.jsp', false);
+    xmlHttp.setRequestHeader('X-Jcms-Ajax-Id', context)
+    xmlHttp.send(null);
+    const htmlResult = xmlHttp.responseText;
+    const el = document.createElement('html');
+    el.innerHTML = htmlResult;
+    return el;
+}
 
-    const urlGetRunningHand = 'https://portail.sdis78.fr/plugins/MainCourantePlugin/jsp/doMciList.jsp'
+function getLatestValidSignature() {
+    let signatureFound = false;
+    let dateProcessed = new Date()
+    let textSignature = ''
+    while (!signatureFound) {
+        console.log(`search for ${dateProcessed.toLocaleDateString('fr')}`)
+        const context = getHtmlDocumentFrom(`https://portail.sdis78.fr/jcms/p_1295618/cs-chevreuse?portlet=p_1336294&dateMci=${dateProcessed.toLocaleDateString('fr').replace('-', '/')}`).getElementsByTagName('body')[0].getAttribute('id');
+        const dataForDay = getDataForContext(context);
 
+        const divs = dataForDay.getElementsByClassName('sdis78box');
+        for (let i = 0; i < divs.length; i++) {
+            let divClasses = divs[i].querySelector('div:nth-child(1)').className;
+            if (divClasses === 'panel signature') {
+                textSignature = divs[i].querySelector('div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > p:nth-child(1)').textContent
+                console.log(textSignature)
+                if (textSignature.match('.*Cumul.*interventions.*: ([0-9]+).*') && textSignature.match('.*Cumul.*infos.*: ([0-9]+).*')) {
+                    signatureFound = true;
+                }
+            }
+        }
+        if (!signatureFound) {
+            dateProcessed.setDate(dateProcessed.getDate() - 1);
+        }
+    }
+    return {
+        lastValidSignDate: dateProcessed,
+        textSignature: textSignature,
+        latestCountInters: extractValue(textSignature, '.*Cumul.*interventions.*: ([0-9]+).*'),
+        latestCountInfos: extractValue(textSignature, '.*Cumul.*infos.*: ([0-9]+).*')
+    }
 
-    let xmlHttpRunningHand = new XMLHttpRequest();
-    xmlHttpRunningHand.open("POST", urlGetRunningHand, false);
-    xmlHttpRunningHand.setRequestHeader('X-Jcms-Ajax-Id', previousDayCtxtId)
-    xmlHttpRunningHand.send(null);
-    let previousDayRunningHandHtml = xmlHttpRunningHand.responseText;
+    // const urlForCtxtId = document.querySelector('div.col-xs-2:nth-child(2) > a:nth-child(1)').getAttribute('href');
+    //
+    // let xmlHttpCtxtId = new XMLHttpRequest();
+    // xmlHttpCtxtId.open("GET", urlForCtxtId, false);
+    // xmlHttpCtxtId.send(null);
+    // let htmlResult = xmlHttpCtxtId.responseText;
+    // let el = document.createElement('html');
+    // el.innerHTML = htmlResult;
+    //
+    // let previousDayCtxtId = el.getElementsByTagName('body')[0].id;
+    //
+    //
+    // console.log(previousDayCtxtId);
+    //
+    // const urlGetRunningHand = 'https://portail.sdis78.fr/plugins/MainCourantePlugin/jsp/doMciList.jsp'
+    //
+    //
+    // let xmlHttpRunningHand = new XMLHttpRequest();
+    // xmlHttpRunningHand.open("POST", urlGetRunningHand, false);
+    // xmlHttpRunningHand.setRequestHeader('X-Jcms-Ajax-Id', previousDayCtxtId)
+    // xmlHttpRunningHand.send(null);
+    // let previousDayRunningHandHtml = xmlHttpRunningHand.responseText;
+    //
+    // let elPreviousDayRunningHandContent = document.createElement('html');
+    // elPreviousDayRunningHandContent.innerHTML = previousDayRunningHandHtml;
+    //
+    //
+    // return elPreviousDayRunningHandContent.getElementsByClassName('signature')[0].getElementsByTagName('p')[0].textContent;
+}
 
-    let elPreviousDayRunningHandContent = document.createElement('html');
-    elPreviousDayRunningHandContent.innerHTML = previousDayRunningHandHtml;
-
-
-    return elPreviousDayRunningHandContent.getElementsByClassName('signature')[0].getElementsByTagName('p')[0].textContent;
+function extractValue(latestSignatureText, regexp) {
+    const match = latestSignatureText.match(regexp);
+    if (match !== null) {
+        return parseInt(match[1]);
+    } else {
+        return null;
+    }
 }
 
 function calculateNewTotal(latestSignatureText, regexp, dailyEvents) {
-    latestSignatureText = 'Consignes passées au Chef de Garde montant.\n' +
-        'Interventions: 6\n' +
-        'Infos: 1\n' +
-        'Cumul annuel interventions : 788\n' +
-        'Cumul annuel infos: 72';
-
     const match = latestSignatureText.match(regexp);
     if (match === null) {
         return 'ECHEC, renseignez les données à la main';
